395a04186d5015583339f9d27dd5c6929bce56b3
Carrotz45 <anisah.johansson@gmail.com>
2025-04-07 14:58:51 +0200

    la till css och js för mina andra sidor och html och css för portfolio

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-var menuButton = document.querySelector(".menu_button");
-var menu = document.querySelector("nav");
+const menuButton = document.querySelector(".menu_button");
+const menu = document.querySelector("nav");
+
+menuButton.addEventListener('click', showMenu);
 
 function showMenu(){
     menu.classList.toggle('OnScreen')
 }
-
-menuButton.addEventListener('click', showMenu());
